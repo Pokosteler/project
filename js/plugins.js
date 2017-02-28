fc3748dd083a313a4c89b205d09c31e154052449
Pokosteler <progarulit.by@yandex.ru>
2017-02-28 12:43:05 +0300

    add project

---
+++ js/plugins.js
@@ -0,0 +1,7 @@
+
+$.backstretch(["images/1329116436-332204-0035132_www.nevseoboi.com.ua.jpg","images/fon_kletki_sploshnoy_cvet_linii_1920x1080.jpg","images/428766_simfoniya_chistogo_tsveta.jpg"], {
+    fade: 1000,
+    duration: 18000
+});
+
+
