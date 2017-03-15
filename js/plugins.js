39e74dafee874e790b0850a3783ff89f42e9062e
Natalia Kozlova <progarulit.by@yandex.ru>
2017-03-15 14:04:17 +0300

    add function

--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,20 @@
-
 $.backstretch(["images/1329116436-332204-0035132_www.nevseoboi.com.ua.jpg","images/fon_kletki_sploshnoy_cvet_linii_1920x1080.jpg","images/428766_simfoniya_chistogo_tsveta.jpg"], {
     fade: 1000,
     duration: 18000
 });
+
+$(function() {
+   $('[name="words"]').on('mousewheel DOMMouseScroll', function(e) {
+     e.preventDefault();
+     var len = this.length,
+       i = this.selectedIndex;
+     if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+       this.selectedIndex = --i == -1 ? len - 1 : i;
+     } else {
+       this.selectedIndex = ++i % len;
+     }
+
+   });
+ })
 
 
